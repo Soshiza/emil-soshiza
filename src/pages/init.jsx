df065bc30e88efa9139ebb44717b74216ac3da31
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-03-17 23:14:45 -0300

    Add animation to InitPage component using Framer Motion
    Integrated Framer Motion library for animation effects in the InitPage component.
    Utilized motion components to animate headings, paragraphs, and images with different transitions and delays.
    Applied custom styles including font, size, color, and margin to enhance the visual appeal of the animated content.

---
+++ src/pages/init.jsx
@@ -0,0 +1,96 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion'; // Importa Framer Motion
+import { Lora } from 'next/font/google';
+
+const lora = Lora({
+    family: 'Lora',
+    subsets: ['latin'],
+    weight: ['400'], 
+    display: 'swap',
+});
+
+const InitPage = () => {
+    const controls = useAnimation();
+
+    useEffect(() => {
+        controls.start('visible'); // Inicia la animación al cargar el componente
+    }, [controls]);
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen  mt-0">
+            <motion.h1
+                className={lora.className}
+                style={{
+                    fontSize: '40px',
+                    fontStyle: 'italic',
+                    marginBottom: '2px',
+                    color: '#808000'
+                }}
+                initial={{ y: -100, opacity: 0 }}
+                animate={controls}
+                variants={{
+                    hidden: { y: -100, opacity: 0 },
+                    visible: { y: 0, opacity: 1 }
+                }}
+                transition={{ duration: 2, delay: 0 }}
+            >
+                Encuentra tu equilibrio
+            </motion.h1>
+            
+            <motion.h2
+                className={lora.className}
+                style={{
+                    fontSize: '18px',
+                    fontStyle: 'italic',
+                    marginBottom: '5px',
+                    color: '#808000',
+                }}
+                initial={{ y: -100, opacity: 0 }}
+                animate={controls}
+                variants={{
+                    hidden: { y: -100, opacity: 0 },
+                    visible: { y: 0, opacity: 1 }
+                }}
+                transition={{ duration: 2, delay: 0.5 }}
+            >
+                Yoga, Masoterapia y Reiki con Antonio Muñoz
+            </motion.h2>
+            
+            <motion.p
+                className={lora.className}
+                style={{
+                    fontSize:'15px',
+                    fontStyle: 'italic',
+                    marginBottom: '20px',
+                    color: '#B9A88F',
+                }}
+                initial={{ y: -100, opacity: 0 }}
+                animate={controls}
+                variants={{
+                    hidden: { y: -100, opacity: 0 },
+                    visible: { y: 0, opacity: 1 }
+                }}
+                transition={{ duration: 1, delay: 1 }}
+            >
+                "Relaja tu cuerpo, libera tu mente y conecta con tu energía interior"
+            </motion.p>
+            
+            <motion.img
+                className="w-auto h-auto mb-4 sm:w-96 md:w-1/2 lg:w-1/3 rounded-lg"
+                src="/portada.webp"
+                alt="Antonio Emil Muñoz"
+                initial={{ y: -100, opacity: 0 }}
+                animate={controls}
+                variants={{
+                    hidden: { y: -100, opacity: 0 },
+                    visible: { y: 0, opacity: 1 }
+                }}
+                transition={{ duration: 1, delay: 2 }}
+            />
+        </div>
+    );
+};
+
+export default InitPage;
