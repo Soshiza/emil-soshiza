d40f0d5df857b036fa5ecee95295ab9cde812075
Zury Martinez <zury.martinez.saldivia@gmail.com>
2024-03-17 23:07:06 -0300

    Add responsive navigation menu to Header component
    Added functionality to toggle the navigation menu for mobile devices using React state.
    Implemented smooth scrolling functionality for navigation links to different sections of the page.
    Ensured that clicking outside of the menu area closes the menu.
    Adjusted styling for responsive display of the navigation menu on small screens.

---
+++ src/components/header.jsx
@@ -0,0 +1,112 @@
+'use client';
+
+import React, { useState, useRef, useEffect } from 'react';
+import Image from 'next/image';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Dancing_Script } from '@next/font/google';
+
+const dancing = Dancing_Script({
+    family: 'Dancing Script',
+    subsets: ['latin'],
+    weight: ['400'],
+    display: 'swap',
+});
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setMenuOpen(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [menuRef]);
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
+    const smoothScroll = (event, target) => {
+        event.preventDefault();
+        document.getElementById(target).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+        closeMenu();
+    };
+
+    return (
+        <header className="bg-rose-pale text-olive p-2 flex items-center justify-between fixed w-full z-50 top-0">
+            <div className="flex items-center">
+                <Image src="/logo.png" alt="Logo" width={45} height={45} className="mr-4" />
+                <h1 className={dancing.className}>
+                    <span style={{ 
+                        fontSize: '35px',
+                        backgroundImage: 'url(/background-gold.webp)',
+                        WebkitBackgroundClip: 'text',
+                        WebkitTextFillColor: 'transparent'
+                    }}>Antonio Emil Muñoz</span>
+                </h1>
+            </div>
+            <div className="md:hidden" ref={menuRef}>
+                <button
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    className="text-olive hover:text-orange-soft focus:outline-none"
+                >
+                    {menuOpen ? <FaTimes /> : <FaBars />}
+                </button>
+            </div>
+            <nav className="hidden md:flex items-center space-x-4">
+                <ul className="md:flex items-center space-x-4">
+                    <li>
+                        <a href="/" className="hover:text-orange-soft" onClick={closeMenu}>Inicio</a>
+                    </li>
+                    <li>
+                        <a href="#beneficios" className="hover:text-orange-soft" onClick={(event) => smoothScroll(event, 'beneficios')}>Beneficios</a>
+                    </li>
+                    <li>
+                        <a href="#acerca-de-mi" className="hover:text-orange-soft" onClick={(event) => smoothScroll(event, 'acerca-de-mi')}>About</a>
+                    </li>
+                    <li>
+                        <a href="#servicios" className="hover:text-orange-soft" onClick={(event) => smoothScroll(event, 'servicios')}>Servicios</a>
+                    </li>
+                    <li>
+                        <a href="#contacto" className="hover:text-orange-soft" onClick={(event) => smoothScroll(event, 'contacto')}>Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+            {menuOpen && (
+                <div className="fixed inset-y-0 right-0 bg-rose-pale p-4 h-80 w-64 pt-24 my-6 rounded-xl text-center" ref={menuRef}>
+                    <nav className="flex flex-col">
+                        <ul>
+                            <li className='mb-2'>
+                                <a href="/" className="hover:text-orange-soft" onClick={closeMenu}>Inicio</a>
+                            </li >
+                            <li className='mb-2'>
+                                <a href="#beneficios" className="hover:text-orange-soft" onClick={(event) => smoothScroll(event, 'beneficios')}>Beneficios</a>
+                            </li>
+                            <li className='mb-2'>
+                                <a href="#acerca-de-mi" className="hover:text-orange-soft" onClick={(event) => smoothScroll(event, 'acerca-de-mi')}>About</a>
+                            </li>
+                            <li className='mb-2'>
+                                <a href="#servicios" className="hover:text-orange-soft" onClick={(event) => smoothScroll(event, 'servicios')}>Servicios</a>
+                            </li>
+                            <li className='mb-2'>
+                                <a href="#contacto" className="hover:text-orange-soft" onClick={(event) => smoothScroll(event, 'contacto')}>Contacto</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            )}
+        </header>
+    );
+};
+
+export default Header;
